ba3ec0aaf2ef3e58ecbc812fa1e462b8b8a0bc0f
JuanJaramillo12004 <juan_edu.jaramillo@uao.edu.co>
2024-10-09 14:40:19 -0500

    Challenge 7

---
+++ 07-challenge/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>My First App in React</h1>
+        <span>
+          Number 10
+        </span>
+      </div>
+    </>
+  )
+}
+
+export default App
